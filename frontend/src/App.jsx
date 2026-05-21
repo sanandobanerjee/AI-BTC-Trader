7aa54ea4faa63c45098cd8d2195a7fcf4608c3ba
sanandobanerjee <sanandobanerjee2@gmail.com>
2026-05-21 13:50:07 +0530

    Implemented Label dominance in frontend components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import Dashboard from "./pages/Dashboard"
 import ErrorBoundary from "./ErrorBoundary"
+import "./index.css"
 import "./dashboard.css"
+import "./components/components.css"
 
 function App() {
   return (
